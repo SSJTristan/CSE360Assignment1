8b2712fec222995fc3da5cf2ea129487f9df3b55
SSJTristan <Tristanealderete@gmail.com>
2020-10-05 13:33:54 -0700

    Creating the original start of the program given to us

---
+++ src/cse360assignment02/AddingMachine.java
@@ -0,0 +1,30 @@
+package cse360assignment02;
+
+
+public class AddingMachine {
+    public static void main(String[]args){
+        System.out.println("Hello world");
+    }
+    private int total;
+
+    public AddingMachine () {
+        total = 0;  // not needed - included for clarity
+    }
+
+    public int getTotal () {
+        return 0;
+    }
+
+    public void add (int value) {
+    }
+
+    public void subtract (int value) {
+    }
+
+    public String toString () {
+        return "";
+    }
+
+    public void clear() {
+    }
+}
